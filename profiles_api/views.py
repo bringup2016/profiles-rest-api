f6c2715655cc50a70b4c3907de60cd65fdb377a8
Yeon <bringup2016@naver.com>
2019-12-26 22:34:01 +0900

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,6 +9,9 @@ from profiles_api import models
 from profiles_api import permissions
 from rest_framework import filters
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -112,3 +115,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
